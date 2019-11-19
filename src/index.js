51b59c31601472b036038298f49b4f294bb5d8a2
Tish Chungoora <tishal82@gmail.com>
2019-11-19 13:58:56 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  populateTodo();
 });
+
+// declare function for intercepting form submission event, populate the todo and reset form
+
+function populateTodo() {
+  const form = document.querySelector("#create-task-form");
+  const desc = document.getElementById("new-task-description");
+  const todoList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(e) {
+
+    // prevent submission from refreshing the page
+    e.preventDefault();
+
+    // make a list element
+    let makeListEl = document.createElement("li");
+
+    // append the list element as a child of the todo list with the description value submitted
+    todoList.appendChild(makeListEl).innerHTML = desc.value
+
+    // reset the form
+    form.reset();
+  });
+}
+
+
+
+
+
+
